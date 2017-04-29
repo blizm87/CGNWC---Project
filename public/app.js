2d35b0a4f787e2757ea0bf10179ea2ba69485e7c
Justin Samuels <blizm87@gmail.com>
2017-04-28 20:04:13 -0700

    styling navbar

--- public/app.js
+++ public/app.js
@@ -11,20 +11,30 @@
       let $tempSlide = $('.tempSlide');
 
       setInterval(function(){
-        for(var i = 0; i < $indSlideContainer.length; i++){
-          $indSlideContainer[i].style.display = 'none';
+        if(slideIndex === 0){
+          $indSlideContainer[slideIndex].style.display = 'block';
+          $tempSlide[0].style.display = 'none';
+          // angular.element($indSlideContainer[slideIndex]).addClass('slideOut');
         }
 
         slideIndex++;
         if(slideIndex > $indSlideContainer.length) {
           slideIndex = 1;
         }
-        $tempSlide[0].style.display = 'none';
-        // angular.element($indSlideContainer[slideIndex-1]).addClass('slideOut');
-        // angular.element($indSlideContainer[slideIndex-1]).addClass('slideIn');
+
+        for(var i = 0; i < $indSlideContainer.length; i++){
+          $indSlideContainer[i].style.display = 'none';
+        }
+        // if(slideIndex-1 === 0){
+        //   $indSlideContainer[slideIndex-1].style.display = 'none';
+        //   $indSlideContainer[slideIndex+2].style.display = 'none';
+        // } else {
+        //     $indSlideContainer[slideIndex-2].style.display = 'none';
+        //   }
+        // angular.element($indSlideContainer[slideIndex-1]).removeClass('slideIn')
         $indSlideContainer[slideIndex-1].style.display = 'block';
-        console.log($indSlideContainer[slideIndex]);
-      }, 3000);
+
+      }, 8000);
 
     }
 })();
