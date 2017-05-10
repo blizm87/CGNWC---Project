dca6793b3b586bfd22754c9a228bbd2ade4669c0
Justin Samuels <blizm87@gmail.com>
2017-05-09 20:50:10 -0700

    test case

--- src/db/seed.js
+++ src/db/seed.js
@@ -25,7 +25,7 @@ member.find({_id: 1234567890}, function(error, member){
 
 
 var cgnwcEvent = new event({
-  _id: 1234567890,
+  _id: 5678901234,
   e_date: 'January 13, 2017',
   e_time: '7:30PM - 9:00PM',
   e_desc: 'Kick Off and Meet & Greet',
@@ -33,7 +33,7 @@ var cgnwcEvent = new event({
   e_add: 'Coconut Grove, FL'
 })
 
-event.find({_id: 1234567890}, function(error, event){
+event.find({_id: 5678901234}, function(error, event){
   if(error) {
     console.log(error)
     cgnwcEvent.save();
