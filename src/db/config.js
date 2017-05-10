49226b9657a61d8ac18ef4d60117d0fc02642534
Justin Samuels <blizm87@gmail.com>
2017-05-09 20:36:45 -0700

    trying to handle promise rejection error

--- src/db/config.js
+++ src/db/config.js
@@ -5,6 +5,9 @@ const url = process.env.MONGODB_URI || 'mongodb://localhost/cgnwc'
 mongoose.connect(url)
 mongoose.connection.once('open', function () {
   console.log(`Mongoose connected to: ${url}`)
+}, function(err){
+  console.log('I am the error');
+  console.log(err)
 })
 
 module.exports = mongoose
