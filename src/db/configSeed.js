53e79050a0b72a9bf52bf83d3740a1adea28b79d
Justin Samuels <blizm87@gmail.com>
2017-07-10 14:24:16 -0700

    testing alternative seed

--- src/db/configSeed.js
+++ src/db/configSeed.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = Promise
-const url = process.env.MONGODB_URI || 'mongodb://localhost/cgnwc'
+const url = process.env.MONGODB_URI || 'mongodb://localhost/cgnwc';
 
 mongoose.connect(url, { useMongoClient: true })
 mongoose.connection.once('open', function () {
--- src/db/seed.js
+++ src/db/seed.js
@@ -1,83 +1,88 @@
-console.log('I am the seed before it runs');
-require('./config.js');
-console.log('I am the seed after requiring configSeed');
 const mongoose = require('mongoose');
-let IdOne = mongoose.Types.ObjectId();
-let strIdOne = IdOne.toString();
-let IdTwo = mongoose.Types.ObjectId();
-let strIdTwo = IdTwo.toString();
-let IdThree = mongoose.Types.ObjectId();
-let strIdThree = IdThree.toString();
-let IdFour = mongoose.Types.ObjectId();
-let strIdFour = IdFour.toString();
-let IdFive = mongoose.Types.ObjectId();
-let strIdFive = IdFive.toString();
-let IdSix = mongoose.Types.ObjectId();
-let strIdSix = IdSix.toString();
+mongoose.Promise = Promise
+const url = process.env.MONGODB_URI || 'mongodb://localhost/cgnwc';
 
+mongoose.connect(url, { useMongoClient: true })
+mongoose.connection.once('open', function () {
+  console.log(`Mongoose connected to: ${url}`)
+  let IdOne = mongoose.Types.ObjectId();
+  let strIdOne = IdOne.toString();
+  let IdTwo = mongoose.Types.ObjectId();
+  let strIdTwo = IdTwo.toString();
+  let IdThree = mongoose.Types.ObjectId();
+  let strIdThree = IdThree.toString();
+  let IdFour = mongoose.Types.ObjectId();
+  let strIdFour = IdFour.toString();
+  let IdFive = mongoose.Types.ObjectId();
+  let strIdFive = IdFive.toString();
+  let IdSix = mongoose.Types.ObjectId();
+  let strIdSix = IdSix.toString();
 
-// REQUIRE MODELS
-const member = require('../models/members.js')
-const event = require('../models/events.js')
+  // REQUIRE MODELS
+  const member = require('../models/members.js')
+  const event = require('../models/events.js')
 
-// POPULATE SCHEMA
-var cgnwcMember = new member({
-  f_name: 'Justin',
-  m_name: 'Kyle',
-  l_name: 'Samuels',
-  fullName: 'Justin Kyle Samuels',
-  title: 'Web Developer',
-  myId: strIdOne
+  // POPULATE SCHEMA
+  var cgnwcMember = new member({
+    f_name: 'Justin',
+    m_name: 'Kyle',
+    l_name: 'Samuels',
+    fullName: 'Justin Kyle Samuels',
+    title: 'Web Developer',
+    myId: strIdOne
+  })
+
+      cgnwcMember.save()
+
+  var cgnwcEvent1 = new event({
+    e_date: 'January 13, 2017',
+    e_time: '7:30PM - 9:00PM',
+    e_desc: 'Kick Off and Meet & Greet',
+    e_loc: 'Ritz Carlton',
+    e_add: 'Coconut Grove, FL',
+    myId: strIdTwo
+  })
+      cgnwcEvent1.save();
+
+  var cgnwcEvent2 = new event({
+    e_date: 'January 21, 2017',
+    e_time: '10:30AM - 1:30PM',
+    e_desc: '“CGNWC” Scholarship Mentee Meeting, Financial Literacy, Social Decorum',
+    e_loc: 'KROMA Art Gallery',
+    e_add: 'Coconut Grove, FL',
+    myId: strIdThree
+  })
+      cgnwcEvent2.save();
+
+  var cgnwcEvent3 = new event({
+    e_date: 'March 18, 2017',
+    e_time: '10:30AM - 1:30PM',
+    e_desc: '“CGNWC” Scholarship Mentee Meeting, Entrepreneurism/Career Readiness, Social Decorum',
+    e_loc: 'ChamberSOUTH (Chamber of Commerce)',
+    e_add: 'South Miami, FL',
+    myId: strIdFour
+  })
+      cgnwcEvent3.save();
+
+  var cgnwcEvent4 = new event({
+    e_date: 'April 15, 2017',
+    e_time: '10:30AM - 1:30PM',
+    e_desc: '“CGNWC” Scholarship Mentee Meeting, Health and Wellness, Social Decorum',
+    e_loc: 'Ritz Carlton',
+    e_add: 'Coconut Grove, FL',
+    myId: strIdFive
+  })
+      cgnwcEvent4.save();
+
+  var cgnwcEvent5 = new event({
+    e_date: 'May 20, 2017',
+    e_time: '10:30AM - 2:30PM',
+    e_desc: '“CGNWC” Scholarship Mentee Meeting Last meeting. This will be an extended meeting to include a review of all covered material. Formal Dining tutorial included.',
+    e_loc: 'Ritz Carlton',
+    e_add: 'Coconut Grove, FL',
+    myId: strIdSix
+  })
+      cgnwcEvent5.save();
 })
 
-    cgnwcMember.save()
-
-var cgnwcEvent1 = new event({
-  e_date: 'January 13, 2017',
-  e_time: '7:30PM - 9:00PM',
-  e_desc: 'Kick Off and Meet & Greet',
-  e_loc: 'Ritz Carlton',
-  e_add: 'Coconut Grove, FL',
-  myId: strIdTwo
-})
-    cgnwcEvent1.save();
-
-var cgnwcEvent2 = new event({
-  e_date: 'January 21, 2017',
-  e_time: '10:30AM - 1:30PM',
-  e_desc: '“CGNWC” Scholarship Mentee Meeting, Financial Literacy, Social Decorum',
-  e_loc: 'KROMA Art Gallery',
-  e_add: 'Coconut Grove, FL',
-  myId: strIdThree
-})
-    cgnwcEvent2.save();
-
-var cgnwcEvent3 = new event({
-  e_date: 'March 18, 2017',
-  e_time: '10:30AM - 1:30PM',
-  e_desc: '“CGNWC” Scholarship Mentee Meeting, Entrepreneurism/Career Readiness, Social Decorum',
-  e_loc: 'ChamberSOUTH (Chamber of Commerce)',
-  e_add: 'South Miami, FL',
-  myId: strIdFour
-})
-    cgnwcEvent3.save();
-
-var cgnwcEvent4 = new event({
-  e_date: 'April 15, 2017',
-  e_time: '10:30AM - 1:30PM',
-  e_desc: '“CGNWC” Scholarship Mentee Meeting, Health and Wellness, Social Decorum',
-  e_loc: 'Ritz Carlton',
-  e_add: 'Coconut Grove, FL',
-  myId: strIdFive
-})
-    cgnwcEvent4.save();
-
-var cgnwcEvent5 = new event({
-  e_date: 'May 20, 2017',
-  e_time: '10:30AM - 2:30PM',
-  e_desc: '“CGNWC” Scholarship Mentee Meeting Last meeting. This will be an extended meeting to include a review of all covered material. Formal Dining tutorial included.',
-  e_loc: 'Ritz Carlton',
-  e_add: 'Coconut Grove, FL',
-  myId: strIdSix
-})
-    cgnwcEvent5.save();
+module.exports = mongoose
